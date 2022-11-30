b72b1149a1d7ea7abe7d6efbdef28b61d454ba3a
lilyOrio <Lili.Li@desaysv.com>
2022-11-30 14:33:19 +0800

    8.2.2给翻页标签栏添加新属性

--- chapter08/app/src/main/java/com/example/myapplication/CustomTabActivity.java
+++ chapter08/app/src/main/java/com/example/myapplication/CustomTabActivity.java
@@ -1,14 +1,46 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class CustomTabActivity extends AppCompatActivity {
+import com.example.myapplication.adapter.ImagePagerAdapter;
+import com.example.myapplication.bean.GoodsInfo;
+
+import java.util.ArrayList;
+
+public class CustomTabActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
+
+    private ArrayList<GoodsInfo> mGoodsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_tab);
+
+        mGoodsList = GoodsInfo.getDefaultList();
+        ImagePagerAdapter adapter = new ImagePagerAdapter(this, mGoodsList);
+
+        ViewPager vp_content = findViewById(R.id.vp_content);
+        vp_content.setAdapter(adapter);
+        vp_content.setCurrentItem(0); // 设置翻页视图显示第一页
+        vp_content.addOnPageChangeListener(this); // 给翻页视图添加页面变更监听器
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Toast.makeText(this, "您翻到的手机品牌是：" + mGoodsList.get(position).name, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 }
