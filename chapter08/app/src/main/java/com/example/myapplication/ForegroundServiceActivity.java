9d52ba9addc850b282876b92c4625adee2b510ed
lilyOrio <91266959+lilyOrio@users.noreply.github.com>
2022-12-04 11:28:27 +0800

    [8.3.3]开启前台服务（模拟音乐播放器）

---
+++ chapter08/app/src/main/java/com/example/myapplication/ForegroundServiceActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.service.MusicService;
+
+public class ForegroundServiceActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText et_song; // 声明一个编辑框对象
+    private Button btn_send_service; // 声明一个按钮对象
+    private boolean isPlaying = true; // 是否正在播放
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foregrand_service);
+        et_song = findViewById(R.id.et_song);
+        btn_send_service = findViewById(R.id.btn_send_service);
+        btn_send_service.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_send_service) {
+            if (TextUtils.isEmpty(et_song.getText())) {
+                Toast.makeText(this, "请填写歌曲名称", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent intent = new Intent(this, MusicService.class);
+            intent.putExtra("is_play",isPlaying);
+            intent.putExtra("song",et_song.getText().toString());
+            startService(intent);
+            isPlaying = !isPlaying;
+            Log.d("lily", "onClick: ");
+        }
+    }
+}
