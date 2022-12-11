40679d4b81d2b2b79e391c7156707588e7327337
lilyOrio <91266959+lilyOrio@users.noreply.github.com>
2022-12-11 22:53:34 +0800

    2022/12/11 9.1.2 自定义标签按钮

--- chapter08/chapter09/src/main/java/com/example/chapter09/TabButtonActivity.java
+++ chapter08/chapter09/src/main/java/com/example/chapter09/TabButtonActivity.java
@@ -3,6 +3,8 @@ package com.example.chapter09;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class TabButtonActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class TabButtonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_button);
+        final TextView tv_select = findViewById(R.id.tv_select);
+        CheckBox ck_tab = findViewById(R.id.ck_tab);
+        // 给复选框设置勾选监听器
+        ck_tab.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.getId() == R.id.ck_tab) {
+                String desc = String.format("标签按钮被%s了", isChecked?"选中":"取消选中");
+                tv_select.setText(desc);
+            }
+        });
     }
 }
