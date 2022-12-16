1122eb381e3a5c67617dcc2cc78d6af777f0046c
‘uidq5850 <Lili.Li@desaysv.com>
2022-12-16 18:10:07 +0800

    2022/12/16 9.5 实战项目：电商首页暂存

---
+++ chapter08/chapter09/src/main/java/com/example/chapter09/fragment/DepartmentClassFragment.java
@@ -0,0 +1,49 @@
+package com.example.chapter09.fragment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chapter09.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DepartmentClassFragment extends Fragment {
+    protected View mView; // 声明一个视图对象
+    protected AppCompatActivity mActivity; // 声明一个活动对象
+    private List<String> mTitleList = new ArrayList<String>(); // 标题文字列表
+
+    public DepartmentClassFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mActivity = (AppCompatActivity) getActivity();
+        mView = inflater.inflate(R.layout.fragment_department_class, container, false);
+        Toolbar tl_head = mView.findViewById(R.id.tl_head);
+        mActivity.setSupportActionBar(tl_head);
+
+        mTitleList.add("服装");
+        mTitleList.add("电器");
+
+        TabLayout tab_title = mView.findViewById(R.id.tab_title);
+        ViewPager2 vp2_content = mView.findViewById(R.id.vp2_content);
+
+
+
+        return mView;
+    }
+}
